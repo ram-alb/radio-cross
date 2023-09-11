d4b90618051bf3f81ccdc98107e8993a77e61f68
Ramil Albakov <ramil.albakov2@gmail.com>
2023-09-11 15:19:40 +0600

    fix: put mail list to environment variables

--- radio_cross/main.py
+++ radio_cross/main.py
@@ -1,3 +1,5 @@
+import os
+
 from anpusr_mail import send_email
 from radio_cross.enm import get_radio_data
 from radio_cross.parser import parse_radio_data
@@ -10,7 +12,7 @@ def make_radio_cross_report():
     radio_data = parse_radio_data(enm_radio_data)
     report_path = fill_report(radio_data)
 
-    to = ['Ramil.Albakov@kcell.kz']
+    to = os.getenv('TO').split(',')
     subject = 'Radio Crosses'
-    message = 'Good day!\n Please find the Radio Cross report in attachment.'
+    message = 'Good day!\nPlease find the Radio Cross report in attachment.'
     send_email(to, subject, message, report_path)
